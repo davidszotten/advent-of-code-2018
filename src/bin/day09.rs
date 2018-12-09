fc9996db263712a6c961bf90fe950ba2831f1ed3
David Szotten <davidszotten@gmail.com>
2018-12-09 18:56:16 +0000

    much more performant solution

--- src/bin/day09.rs
+++ src/bin/day09.rs
@@ -1,4 +1,5 @@
 use aoc2018::{dispatch, Result};
+use std::collections::VecDeque;
 
 fn main() {
     dispatch(&part1, &part2)
@@ -6,37 +7,39 @@ fn main() {
 
 #[derive(Debug)]
 struct Marbles {
-    marbles: Vec<u64>,
+    marbles: VecDeque<u64>,
     current_pos: usize,
 }
 
 impl Marbles {
     fn new() -> Self {
+        let mut marbles = VecDeque::new();
+        marbles.push_back(0);
         Marbles {
-            marbles: vec![0],
+            marbles: marbles,
             current_pos: 0,
         }
     }
 
     fn insert(&mut self, next: u64) {
-        let next_pos = (self.current_pos + 2) % self.marbles.len();
-        self.marbles.insert(next_pos, next);
-        self.current_pos = next_pos;
+        let val = self.marbles.pop_front().unwrap();
+        self.marbles.push_back(val);
+        self.marbles.push_back(next);
     }
 
     fn remove(&mut self) -> u64 {
-        let mut remove_num = (self.current_pos as i64 - 7) % self.marbles.len() as i64;
-        if remove_num < 0 {
-            remove_num += self.marbles.len() as i64;
+        for _ in 0..7 {
+            let val = self.marbles.pop_back().unwrap();
+            self.marbles.push_front(val);
         }
-        let remove_pos = remove_num as usize;
-        self.current_pos = remove_pos;
-        self.marbles.remove(remove_pos)
+        let removed = self.marbles.pop_back().unwrap();
+        let val = self.marbles.pop_front().unwrap();
+        self.marbles.push_back(val);
+        removed
     }
 }
 
 fn place(marbles: &mut Marbles, next: u64) -> u64 {
-    // println!("{:?}", marbles);
     if next > 0 && next % 23 == 0 {
         return next + marbles.remove();
     } else {
@@ -49,7 +52,7 @@ fn play(n_players: usize, n_marbles: u64) -> u64 {
     let mut scores = vec![0; n_players];
     let mut current_player = 0;
     let mut marbles = Marbles::new();
-    for next in 1..n_marbles {
+    for next in 1..=n_marbles {
         scores[current_player] += place(&mut marbles, next);
         current_player = (current_player + 1) % n_players;
     }
@@ -57,7 +60,10 @@ fn play(n_players: usize, n_marbles: u64) -> u64 {
 }
 
 fn part1(input: &str) -> Result<u64> {
-    let numbers: Vec<u64> = input.split_whitespace().filter_map(|x| x.parse().ok()).collect();
+    let numbers: Vec<u64> = input
+        .split_whitespace()
+        .filter_map(|x| x.parse().ok())
+        .collect();
     Ok(play(numbers[0] as usize, numbers[1]))
 }
 
@@ -71,7 +77,10 @@ mod tests {
 
     #[test]
     fn test_part1() -> Result<()> {
-        Ok(assert_eq!(part1("9 players; last marble is worth 25 points")?, 32))
+        Ok(assert_eq!(
+            part1("9 players; last marble is worth 25 points")?,
+            32
+        ))
     }
 
     #[test]
@@ -83,7 +92,7 @@ mod tests {
     fn test_play2() {
         assert_eq!(play(10, 1618), 8317);
         assert_eq!(play(13, 7999), 146373);
-        // assert_eq!(play(17, 1104), 2764);
+        assert_eq!(play(17, 1104), 2764);
         assert_eq!(play(21, 6111), 54718);
         assert_eq!(play(30, 5807), 37305);
     }
