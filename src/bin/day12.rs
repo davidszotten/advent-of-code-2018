96ed7ef5a6f665e3a572b753d6cccb08be2659ea
David Szotten <davidszotten@gmail.com>
2018-12-13 22:28:05 +0000

    day 12 part 1

---
+++ src/bin/day12.rs
@@ -0,0 +1,94 @@
+use aoc2018::{dispatch, Result};
+use failure::{Error};
+use std::collections::HashMap;
+use std::str::FromStr;
+
+fn main() {
+    dispatch(&part1, &part2)
+}
+
+struct Rule {
+    before: Vec<bool>,
+    after: bool,
+}
+
+fn char_to_bool(c: char) -> Option<bool> {
+    match c {
+        '.' => Some(false),
+        '#' => Some(true),
+        _ => None
+    }
+}
+
+impl FromStr for Rule {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self> {
+        let mut parts = s.split_whitespace();
+        let left: &str = parts.nth(0).expect("left");
+        let right = parts.nth(1).expect("right");
+
+        let before = left.chars().filter_map(char_to_bool).collect::<Vec<_>>();
+        let after = right.chars().filter_map(char_to_bool).nth(0).expect("after");
+
+        Ok(Rule { before, after})
+    }
+}
+
+fn part1(input: &str) -> Result<i32> {
+    let mut rows = input.split('\n');
+    let initial = rows.next().expect("next").split_whitespace().nth(2).expect("1").chars().filter_map(char_to_bool).collect::<Vec<_>>();
+    rows.next();
+    let rules: Vec<Rule> = rows.filter_map(|row| row.parse().ok()).collect();
+    let rule_map: HashMap<_, bool> = rules.iter().map(|r| (&r.before[..], r.after)).collect();
+    // println!("{:?}", rule_map);
+    // println!("{:?}", initial.iter().map(|&b| if b {'#'} else {'.'}).collect::<String>());
+    let mut pots = vec![false;60];
+    pots.extend(&initial);
+    pots.extend(&vec![false; 60]);
+    // println!("{}", pots.len());
+    for _ in 0..20 {
+        // fn foo(w: Vec<char>) -> char {
+        let foo = |w| {
+            // println!("{:?}", w);
+            *rule_map.get(&w).unwrap_or(&false)
+        };
+        pots = pots.windows(5).map(foo).collect();
+        // initial = initial.windows(5).map(|w| .expect("missing")).collect();
+        // println!("{}", pots.len());
+    }
+    // println!("{}", pots.iter().map(|&b| if b {'#'} else {'.'}).collect::<String>());
+    // println!("{:?}", (-20..(20+initial.len() as i32)).zip(pots.clone()).map(|(i, p)| if p {i} else {0}).collect::<Vec<_>>());
+    Ok((-20..(20+initial.len() as i32)).zip(pots).map(|(i, p)| if p {i} else {0}).sum())
+}
+
+fn part2(_input: &str) -> Result<i32> {
+    Ok(0)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INPUT: &str = "initial state: #..#.#..##......###...###
+
+...## => #
+..#.. => #
+.#... => #
+.#.#. => #
+.#.## => #
+.##.. => #
+.#### => #
+#.#.# => #
+#.### => #
+##.#. => #
+##.## => #
+###.. => #
+###.# => #
+####. => #";
+
+    #[test]
+    fn test_part1() -> Result<()> {
+        Ok(assert_eq!(part1(INPUT)?, 325))
+    }
+}
