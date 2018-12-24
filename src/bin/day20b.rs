91c82ef83c73b12f04b2720844d810cbf837eb2d
David Szotten <davidszotten@gmail.com>
2018-12-24 21:41:44 +0000

    day20 attempt 2

--- src/bin/day20b.rs
+++ src/bin/day20b.rs
@@ -1,19 +1,86 @@
 use aoc2018::{dispatch, Result};
 use failure::{err_msg, Error};
+use std::collections::HashSet;
+use std::fmt;
+use std::ops;
 use std::str::FromStr;
 
 fn main() {
     dispatch(&part1, &part2)
 }
 
-fn part1(_input: &str) -> Result<i32> {
-    Ok(0)
+fn part1(input: &str) -> Result<usize> {
+    let pattern: Pattern = input.parse()?;
+    println!("{:?}", pattern);
+    let seen = pattern.walk();
+    println!("{:?}", seen);
+    let min_x = seen.iter().map(|(Coor { x, y: _ }, _)| *x).min().unwrap();
+    let max_x = seen.iter().map(|(Coor { x, y: _ }, _)| *x).max().unwrap();
+    let min_y = seen.iter().map(|(Coor { x: _, y }, _)| *y).min().unwrap();
+    let max_y = seen.iter().map(|(Coor { x: _, y }, _)| *y).max().unwrap();
+    println!("{}/{}, {}/{}", min_x, max_x, min_y, max_y);
+    for y in min_y..=max_y {
+        for x in min_x..=max_x {
+            let north = seen.contains(&(Coor{x, y}, 'N'));
+            let south = seen.contains(&(Coor{x, y}, 'S'));
+            let east = seen.contains(&(Coor{x, y}, 'E'));
+            let west = seen.contains(&(Coor{x, y}, 'W'));
+            let found = north || south || east || west;
+            let hmarker = |b| if b {"|"} else {"?"};
+            let cmarker = if (x, y) == (0, 0) {
+                "X"
+            } else if found {
+                "."
+            } else { " " };
+            print!("{}{}{}", hmarker(west), cmarker, hmarker(east));
+        }
+        println!("");
+    }
+    Ok(seen.len())
 }
 
 fn part2(_input: &str) -> Result<i32> {
     Ok(0)
 }
 
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
+struct Coor {
+    x: i32,
+    y: i32,
+}
+
+impl fmt::Debug for Coor {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "({}, {})", self.x, self.y)
+    }
+}
+
+impl ops::Add for Coor {
+    type Output = Coor;
+
+    fn add(self, other: Coor) -> Self::Output {
+        Coor {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+
+impl Coor {
+    fn new() -> Self {
+        Coor { x: 0, y: 0 }
+    }
+    fn from_char(c: char) -> Self {
+        let (x, y) = match c {
+            'N' => (0, 1),
+            'S' => (0, -1),
+            'E' => (-1, 0),
+            'W' => (1, 0),
+            _ => panic!("bad from_char"),
+        };
+        Coor { x, y }
+    }
+}
 
 #[derive(Debug, Clone, PartialEq)]
 enum Pattern {
@@ -23,11 +90,51 @@ enum Pattern {
     Partial(String),
 }
 
+impl Pattern {
+    fn walk(&self) -> HashSet<(Coor, char)> {
+        fn inner_walk(
+            pattern: &Pattern,
+            mut seen: HashSet<(Coor, char)>,
+            mut pos: Coor,
+        ) -> (HashSet<(Coor, char)>, Coor) {
+            use self::Pattern::*;
+            match pattern {
+                Literal(s) => {
+                    for c in s.chars() {
+                        let direction = Coor::from_char(c);
+                        seen.insert((pos, c));
+                        pos = pos + direction;
+                    }
+                }
+                Concat(v) => {
+                    for p in v {
+                        let ret = inner_walk(p, seen, pos);
+                        seen = ret.0;
+                        pos = ret.1;
+                    }
+                }
+                Or(v) => {
+                    for p in v {
+                        let ret = inner_walk(p, seen, pos);
+                        seen = ret.0;
+                        // pos = ret.1;
+                    }
+                }
+                Partial(_) => panic!("can't walk partial"),
+            }
+            (seen, pos)
+        }
+        let seen = HashSet::new();
+        let pos = Coor::new();
+        inner_walk(self, seen, pos).0
+    }
+}
+
 impl FromStr for Pattern {
     type Err = Error;
 
     fn from_str(s: &str) -> Result<Self> {
-        println!("parsing `{}`", s);
+        // println!("parsing `{}`", s);
 
         use self::Pattern::*;
         let mut res: Vec<Pattern> = vec![];
@@ -55,7 +162,7 @@ impl FromStr for Pattern {
                         }
                         if c == ')' {
                             if level == 0 {
-                                break
+                                break;
                             }
                             level -= 1;
                         }
@@ -96,7 +203,7 @@ impl FromStr for Pattern {
                         current = vec![];
                     }
                 }
-                _ => Err(err_msg("parse fail"))?,
+                c => Err(err_msg(format!("parse fail: `{}`", c)))?,
             }
         }
 
@@ -109,8 +216,8 @@ impl FromStr for Pattern {
             res.push(Partial(current.iter().collect()));
         }
 
-        let mode_str = if or_mode { "or" } else { "concat" };
-        println!("res ({}): {:?}", mode_str, res);
+        // let mode_str = if or_mode { "or" } else { "concat" };
+        // println!("res ({}): {:?}", mode_str, res);
         let inside = res
             .into_iter()
             .map(|p| match p {
@@ -122,7 +229,7 @@ impl FromStr for Pattern {
             .collect();
         let complete = if or_mode { Or(inside) } else { Concat(inside) };
 
-        println!("complete: {:?}", complete);
+        // println!("complete: {:?}", complete);
         Ok(complete)
     }
 }
