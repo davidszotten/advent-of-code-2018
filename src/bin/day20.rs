311a89031a8069c079a67344d61e197c23e33dde
David Szotten <davidszotten@gmail.com>
2018-12-25 21:31:23 +0000

    new attempt more promising

--- src/bin/day20.rs
+++ src/bin/day20.rs
@@ -1,111 +1,239 @@
 use aoc2018::{dispatch, Result};
-use std::mem;
+use failure::{err_msg, Error};
+use std::collections::HashSet;
+use std::fmt;
+use std::ops;
+use std::str::FromStr;
 
 fn main() {
     dispatch(&part1, &part2)
 }
 
-fn indent(level: usize) {
-    for _ in 0..level {
-        print!("\t");
-    }
-}
-
-// ^E(N|S)W$
-// ^E(N|S(E|N))$
-fn walk(input: &str, level: usize, so_far: Vec<Vec<char>>) -> (usize, Vec<Vec<char>>) {
-    let mut res: Vec<Vec<char>> = so_far;
-    let mut current: Vec<char> = vec![];
-    let mut skip = 0;
-    let mut consumed = 0;
-
-    for (idx, c) in input.chars().enumerate() {
-        if skip > 0 {
-            indent(level);
-            println!("skipping {}", c);
-            skip -= 1;
-            continue;
+fn part1(input: &str) -> Result<usize> {
+    let pattern: Pattern = input.parse()?;
+    println!("{:?}", pattern);
+    let seen = pattern.walk();
+    println!("{:?}", seen);
+    let min_x = seen.iter().map(|(Coor { x, y: _ }, _)| *x).min().unwrap();
+    let max_x = seen.iter().map(|(Coor { x, y: _ }, _)| *x).max().unwrap();
+    let min_y = seen.iter().map(|(Coor { x: _, y }, _)| *y).min().unwrap();
+    let max_y = seen.iter().map(|(Coor { x: _, y }, _)| *y).max().unwrap();
+    println!("{}/{}, {}/{}", min_x, max_x, min_y, max_y);
+    for y in min_y..=max_y {
+        for x in min_x..=max_x {
+            let north = seen.contains(&(Coor { x, y }, 'N'));
+            let south = seen.contains(&(Coor { x, y }, 'S'));
+            let east = seen.contains(&(Coor { x, y }, 'E'));
+            let west = seen.contains(&(Coor { x, y }, 'W'));
+            let found = north || south || east || west;
+            let hmarker = |b| if b { "|" } else { "?" };
+            let cmarker = if (x, y) == (0, 0) {
+                "X"
+            } else if found {
+                "."
+            } else {
+                " "
+            };
+            print!("{}{}{}", hmarker(west), cmarker, hmarker(east));
         }
-        consumed += 1;
-        match c {
-            '^' => {}
-            c @ 'N' | c @ 'S' | c @ 'E' | c @ 'W' => {
-                current.push(c);
-            }
-            '$' => {
-                indent(level);
-                println!("$: {:?} {:?}", res, current);
-                if res.len() > 0 {
-                    for entry in res.iter_mut() {
-                        entry.extend(current.clone());
-                    }
-                    current = vec![];
-                } else {
-                    res.push(current);
-                    current = vec![];
-                }
-                indent(level);
-                println!("$: {:?} {:?}", res, current);
-            }
-            '(' => {
-                for entry in res.iter_mut() {
-                    entry.extend(current.clone());
-                }
-                indent(level);
-                println!("( {:?}", res);
-                current = vec![];
-
-                let mut new_res = vec![];
-                let (to_skip, walked) = walk(&input[idx + 1..], level + 1, vec![]);
-                skip += to_skip;
-                indent(level);
-                println!("walked: {:?}", walked);
-                for option in walked.into_iter() {
-                    for mut a in res.clone() {
-                        indent(level);
-                        println!("have a: {:?}, option: {:?}", a, option);
-                        a.extend(option.clone());
-                        indent(level);
-                        println!("push {:?}", a);
-                        new_res.push(a);
-                    }
-                }
-                mem::swap(&mut new_res, &mut res);
-                indent(level);
-                println!("copied: {:?}", res);
-            }
-            ')' => {
-                indent(level);
-                println!("closing bracket on lvl {}", level);
-                break;
-            }
-            '|' => {
-                indent(level);
-                println!("before | {:?}", res);
-                res.push(current);
-                current = vec![];
-                indent(level);
-                println!("after | {:?}", res);
-            }
-            _ => unreachable!(),
-        }
+        println!("");
     }
-
-    if current.len() > 0 {
-        res.push(current);
-    }
-    indent(level);
-    println!("returning lvl {} ({}, {:?})", level, consumed, res);
-    (consumed, res)
-}
-
-fn part1(input: &str) -> Result<i32> {
-    walk(input, 0, vec![vec![]]);
-    Ok(0)
+    Ok(seen.len())
 }
 
 fn part2(_input: &str) -> Result<i32> {
     Ok(0)
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
+struct Coor {
+    x: i32,
+    y: i32,
+}
+
+impl fmt::Debug for Coor {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "({}, {})", self.x, self.y)
+    }
+}
+
+impl ops::Add for Coor {
+    type Output = Coor;
+
+    fn add(self, other: Coor) -> Self::Output {
+        Coor {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+
+impl Coor {
+    fn new() -> Self {
+        Coor { x: 0, y: 0 }
+    }
+    fn from_char(c: char) -> Self {
+        let (x, y) = match c {
+            'N' => (0, 1),
+            'S' => (0, -1),
+            'E' => (-1, 0),
+            'W' => (1, 0),
+            _ => panic!("bad from_char"),
+        };
+        Coor { x, y }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+enum Pattern {
+    Literal(String),
+    Concat(Vec<Pattern>),
+    Or(Vec<Pattern>),
+    Partial(String),
+}
+
+impl Pattern {
+    fn walk(&self) -> HashSet<(Coor, char)> {
+        fn inner_walk(
+            pattern: &Pattern,
+            mut seen: HashSet<(Coor, char)>,
+            mut pos: Coor,
+        ) -> (HashSet<(Coor, char)>, Coor) {
+            use self::Pattern::*;
+            match pattern {
+                Literal(s) => {
+                    for c in s.chars() {
+                        let direction = Coor::from_char(c);
+                        seen.insert((pos, c));
+                        pos = pos + direction;
+                    }
+                }
+                Concat(v) => {
+                    for p in v {
+                        let ret = inner_walk(p, seen, pos);
+                        seen = ret.0;
+                        pos = ret.1;
+                    }
+                }
+                Or(v) => {
+                    for p in v {
+                        let ret = inner_walk(p, seen, pos);
+                        seen = ret.0;
+                        // pos = ret.1;
+                    }
+                }
+                Partial(_) => panic!("can't walk partial"),
+            }
+            (seen, pos)
+        }
+        let seen = HashSet::new();
+        let pos = Coor::new();
+        inner_walk(self, seen, pos).0
+    }
+}
+
+impl FromStr for Pattern {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self> {
+        // println!("parsing `{}`", s);
+
+        use self::Pattern::*;
+        let mut res: Vec<Pattern> = vec![];
+        let mut current = vec![];
+        let mut or_mode = false;
+        let mut chars = s.chars();
+
+        while let Some(c) = chars.next() {
+            match c {
+                '^' | '$' => {}
+                c @ 'N' | c @ 'S' | c @ 'E' | c @ 'W' => {
+                    current.push(c);
+                }
+                '(' => {
+                    if !current.is_empty() {
+                        res.push(Partial(current.iter().collect()));
+                        current = vec![];
+                    }
+
+                    let mut pending = vec![];
+                    let mut level = 0;
+                    while let Some(c) = chars.next() {
+                        if c == '(' {
+                            level += 1;
+                        }
+                        if c == ')' {
+                            if level == 0 {
+                                break;
+                            }
+                            level -= 1;
+                        }
+                        pending.push(c);
+                    }
+                    if !pending.is_empty() {
+                        res.push(Partial(pending.iter().collect()));
+                    }
+                }
+                '|' => {
+                    or_mode = true;
+                    if !current.is_empty() {
+                        res.push(Partial(current.iter().collect()));
+                        current = vec![];
+                    }
+                    let mut pending = vec![];
+                    let mut level = 0;
+                    while let Some(c) = chars.next() {
+                        if c == '(' {
+                            level += 1;
+                        }
+                        if c == ')' {
+                            if level == 0 {
+                                res.push(Partial(pending.iter().collect()));
+                                pending = vec![];
+                            }
+                            level -= 1;
+                        }
+                        pending.push(c);
+                    }
+                    if !pending.is_empty() {
+                        res.push(Partial(pending.iter().collect()));
+                    }
+                }
+                ')' => {
+                    if !current.is_empty() {
+                        res.push(Partial(current.iter().collect()));
+                        current = vec![];
+                    }
+                }
+                c => Err(err_msg(format!("parse fail: `{}`", c)))?,
+            }
+        }
+
+        let is_literal = |s: &str| {
+            s.chars()
+                .all(|c| c == 'N' || c == 'S' || c == 'E' || c == 'W')
+        };
+
+        if !current.is_empty() {
+            res.push(Partial(current.iter().collect()));
+        }
+
+        // let mode_str = if or_mode { "or" } else { "concat" };
+        // println!("res ({}): {:?}", mode_str, res);
+        let inside = res
+            .into_iter()
+            .map(|p| match p {
+                Partial(ref s) if is_literal(&s) => Literal(s.clone()),
+                Partial(s) => s.parse().expect("sub parsing"),
+                p => p,
+            })
+            // .filter(|p| if let Literal(s) = p && s == "" {false} else {true})
+            .collect();
+        let complete = if or_mode { Or(inside) } else { Concat(inside) };
+
+        // println!("complete: {:?}", complete);
+        Ok(complete)
+    }
 }
 
 #[cfg(test)]
@@ -113,37 +241,42 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_walk1() {
-        assert_eq!(walk("^WNE$", 0, vec![]).1, vec![vec!['W', 'N', 'E']]);
+    fn test_parse1() -> Result<()> {
+        use self::Pattern::*;
+        Ok(assert_eq!(
+            "^SNEW$".parse::<Pattern>()?,
+            Concat(vec![Literal("SNEW".into())])
+        ))
     }
-
     #[test]
-    fn test_walk2() {
-        assert_eq!(
-            walk("^E(N|S)W$", 0, vec![vec![]]).1,
-            vec![vec!['E', 'N', 'W'], vec!['E', 'S', 'W']]
-        );
+    fn test_parse2() -> Result<()> {
+        use self::Pattern::*;
+        Ok(assert_eq!(
+            "^E(N|S)W$".parse::<Pattern>()?,
+            Concat(vec![
+                Literal("E".into()),
+                Or(vec![Literal("N".into()), Literal("S".into())]),
+                Literal("W".into()),
+            ])
+        ))
     }
-
     #[test]
-    fn test_walk3() {
-        assert_eq!(
-            walk("^E(N|S(E|N))$", 0, vec![vec![]]).1,
-            vec![vec!['E', 'N'], vec!['E', 'S', 'E'], vec!['E', 'S', 'N'],]
-        );
+    fn test_parse3() -> Result<()> {
+        use self::Pattern::*;
+        Ok(assert_eq!(
+            "^E(N|S(E|N))$".parse::<Pattern>()?,
+            Concat(vec![
+                Literal("E".into()),
+                Or(vec![
+                    Literal("N".into()),
+                    Concat(vec![
+                        Literal("S".into()),
+                        Or(vec![Literal("E".into()), Literal("N".into())]),
+                    ]),
+                ])
+            ])
+        ))
     }
-
-    // #[test]
-    // fn test_walk4() {
-    //     assert_eq!(
-    //         walk("^ENWWW(NEEE|SSE(EE|N))$", 0, vec![vec![]]).1,
-    //         vec![
-    //             vec!['E', 'N',  'W', 'W', 'W', 'N', 'E', 'E', 'E'],
-    //             vec!['E', 'N',  'W', 'W', 'W', 'S', 'S', 'E', 'E', 'E'],
-    //             vec!['E', 'N',  'W', 'W', 'W', 'S', 'S', 'E', 'I', 'N'],
-    //         ]
-    //     );
-    // }
 
     #[test]
     fn test_part1() -> Result<()> {
