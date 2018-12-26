c1b43e79e34bb7b7cba1df68b057060233268032
David Szotten <davidszotten@gmail.com>
2018-12-26 14:41:14 +0000

    memoize

--- src/bin/day22.rs
+++ src/bin/day22.rs
@@ -1,54 +1,56 @@
 use aoc2018::{dispatch, Result};
-use std::collections::HashMap;
-// use cached::cached;
+use cached::cached;
 
 fn main() {
     dispatch(&part1, &part2)
 }
 
-enum Type {
-    Rocky,
-    Wet,
-    Narrow,
-}
+// enum Type {
+//     Rocky,
+//     Wet,
+//     Narrow,
+// }
 
-impl Type {
-    fn new(erosion: u32) -> Self {
-        use self::Type::*;
-        match erosion % 3 {
-            0 => Rocky,
-            1 => Wet,
-            2 => Narrow,
-            _ => panic!("bad modulo"),
-        }
-    }
+// impl Type {
+//     fn new(erosion: u32) -> Self {
+//         use self::Type::*;
+//         match erosion % 3 {
+//             0 => Rocky,
+//             1 => Wet,
+//             2 => Narrow,
+//             _ => panic!("bad modulo"),
+//         }
+//     }
 
-    fn risk(&self) -> u32 {
-        use self::Type::*;
-        match self {
-            Rocky => 0,
-            Wet => 1,
-            Narrow => 2,
-        }
-    }
-}
+//     fn risk(&self) -> u32 {
+//         use self::Type::*;
+//         match self {
+//             Rocky => 0,
+//             Wet => 1,
+//             Narrow => 2,
+//         }
+//     }
+// }
 
 fn erosion(depth: u32, geo_index: u32) -> u32 {
     (geo_index + depth) % 20183
 }
 
-fn geo_index(depth: u32, target: (u32, u32), x: u32, y: u32) -> u32 {
-    if (x, y) == (0, 0) {
-        0
-    } else if (x, y) == target {
-        0
-    } else if y == 0 {
-        x * 16807
-    } else if x == 0 {
-        y * 48271
-    } else {
-        erosion(depth, geo_index(depth, target, x - 1, y))
-            * erosion(depth, geo_index(depth, target, x, y - 1))
+cached! {
+    GEO_INDEX;
+    fn geo_index(depth: u32, target: (u32, u32), x: u32, y: u32) -> u32 = {
+        if (x, y) == (0, 0) {
+            0
+        } else if (x, y) == target {
+            0
+        } else if y == 0 {
+            x * 16807
+        } else if x == 0 {
+            y * 48271
+        } else {
+            erosion(depth, geo_index(depth, target, x - 1, y))
+                * erosion(depth, geo_index(depth, target, x, y - 1))
+        }
     }
 }
 
@@ -57,27 +59,11 @@ fn calculate(depth: u32, target: (u32, u32)) -> u32 {
 
     let mut risk = 0;
 
-    let get_erosion = |geo_index: u32| (geo_index + depth) % 20183;
-
-    let mut erosion_map = HashMap::new();
-
     for y in 0..=target_y {
         for x in 0..=target_x {
-            let geo_index = if (x, y) == (0, 0) {
-                0
-            } else if (x, y) == target {
-                0
-            } else if y == 0 {
-                x * 16807
-            } else if x == 0 {
-                y * 48271
-            } else {
-                erosion_map.get(&(x - 1, y)).expect("coor missing")
-                    * erosion_map.get(&(x, y - 1)).expect("coor missing")
-            };
-            let erosion = get_erosion(geo_index);
-            erosion_map.insert((x, y), erosion);
-            risk += erosion % 3;
+            let gi = geo_index(depth, target, x, y);
+            let er = erosion(depth, gi);
+            risk += er % 3;
         }
     }
 
