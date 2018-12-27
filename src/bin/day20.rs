034f4fc65385c5e5b17ac97eb1d62164955d981b
David Szotten <davidszotten@gmail.com>
2018-12-27 18:18:54 +0000

    day 20 attemp 3

--- src/bin/day20.rs
+++ src/bin/day20.rs
@@ -1,5 +1,6 @@
 use aoc2018::{dispatch, Result};
-use failure::{err_msg, Error};
+// use failure::{err_msg, Error};
+use failure::{Error};
 use std::collections::HashSet;
 use std::fmt;
 use std::ops;
@@ -136,104 +137,200 @@ impl FromStr for Pattern {
     type Err = Error;
 
     fn from_str(s: &str) -> Result<Self> {
-        // println!("parsing `{}`", s);
-
         use self::Pattern::*;
-        let mut res: Vec<Pattern> = vec![];
-        let mut current = vec![];
-        let mut or_mode = false;
-        let mut chars = s.chars();
+        let mut chunks = vec![];
+        let mut chunk_start = 0;
+        let mut literal_chunks = vec![];
+        let mut literal_start = 0;
+        let mut or_positions = vec![];
+        let mut bracket_chunks = vec![];
+        let mut bracket_start = 0;
+        let mut pos = 0;
 
+        let mut bracket = 0;
+
+        let mut chars = s.chars();
         while let Some(c) = chars.next() {
             match c {
-                '^' | '$' => {}
-                c @ 'N' | c @ 'S' | c @ 'E' | c @ 'W' => {
-                    current.push(c);
-                }
                 '(' => {
-                    if !current.is_empty() {
-                        res.push(Partial(current.iter().collect()));
-                        current = vec![];
-                    }
-
-                    let mut pending = vec![];
-                    let mut level = 0;
-                    while let Some(c) = chars.next() {
-                        if c == '(' {
-                            level += 1;
+                    if bracket == 0 {
+                        bracket_start = pos;
+                        if literal_start != pos {
+                            literal_chunks.push((literal_start, pos));
                         }
-                        if c == ')' {
-                            if level == 0 {
-                                break;
-                            }
-                            level -= 1;
+                        if chunk_start != pos {
+                            chunks.push((chunk_start, pos));
+                            chunk_start = pos;
                         }
-                        pending.push(c);
                     }
-                    if !pending.is_empty() {
-                        res.push(Partial(pending.iter().collect()));
-                    }
-                }
-                '|' => {
-                    or_mode = true;
-                    if !current.is_empty() {
-                        res.push(Partial(current.iter().collect()));
-                        current = vec![];
-                    }
-                    let mut pending = vec![];
-                    let mut level = 0;
-                    while let Some(c) = chars.next() {
-                        if c == '(' {
-                            level += 1;
-                        }
-                        if c == ')' {
-                            if level == 0 {
-                                res.push(Partial(pending.iter().collect()));
-                                pending = vec![];
-                            }
-                            level -= 1;
-                        }
-                        pending.push(c);
-                    }
-                    if !pending.is_empty() {
-                        res.push(Partial(pending.iter().collect()));
-                    }
-                }
+                    bracket += 1;
+                },
                 ')' => {
-                    if !current.is_empty() {
-                        res.push(Partial(current.iter().collect()));
-                        current = vec![];
+                    bracket -= 1;
+                    if bracket == 0 {
+                        chunks.push((chunk_start, pos + 1));
+                        bracket_chunks.push((bracket_start, pos + 1));
+                        literal_start = pos + 1;
+                        chunk_start = pos + 1;
                     }
-                }
-                c => Err(err_msg(format!("parse fail: `{}`", c)))?,
+                },
+                '|' => {
+                    if bracket == 0 {
+                        or_positions.push(pos);
+                    }
+                },
+                _ => {},
             }
-        }
-
-        let is_literal = |s: &str| {
-            s.chars()
-                .all(|c| c == 'N' || c == 'S' || c == 'E' || c == 'W')
+            pos += 1;
         };
 
-        if !current.is_empty() {
-            res.push(Partial(current.iter().collect()));
+        if literal_start != pos {
+            literal_chunks.push((literal_start, pos));
+        }
+        if chunk_start != pos {
+            chunks.push((chunk_start, pos));
         }
 
-        // let mode_str = if or_mode { "or" } else { "concat" };
-        // println!("res ({}): {:?}", mode_str, res);
-        let inside = res
-            .into_iter()
-            .map(|p| match p {
-                Partial(ref s) if is_literal(&s) => Literal(s.clone()),
-                Partial(s) => s.parse().expect("sub parsing"),
-                p => p,
-            })
-            // .filter(|p| if let Literal(s) = p && s == "" {false} else {true})
-            .collect();
-        let complete = if or_mode { Or(inside) } else { Concat(inside) };
+        println!("{}", s);
+        println!("or: {:?}", or_positions);
+        let mut ostart = 0;
+        for &opos in or_positions.iter() {
+            println!("\t{:?}", &s[ostart..opos]);
+            ostart = opos + 1;
+        }
+        println!("\t{:?}", &s[ostart..]);
 
-        // println!("complete: {:?}", complete);
-        Ok(complete)
+        // println!("literals: {:?}", literal_chunks);
+        // for (start, end) in literal_chunks {
+        //     println!("\t{:?}", &s[start..end]);
+        // }
+        // println!("brackets: {:?}", bracket_chunks);
+        // for &(start, end) in bracket_chunks.iter() {
+        //     println!("\t{:?}", &s[start..end]);
+        // }
+        println!("chunks: {:?}", chunks);
+        for &(start, end) in chunks.iter() {
+            println!("\t{:?}", &s[start..end]);
+        }
+
+        if !or_positions.is_empty() {
+            let mut ors: Vec<Pattern> = vec![];
+
+            let mut ostart = 0;
+            for &opos in or_positions.iter() {
+                ors.push(s[ostart..opos].parse()?);
+                ostart = opos + 1;
+            }
+
+            return Ok(Or(ors));
+        }
+
+        for (start, end) in bracket_chunks {
+            &s[start+1..end-1].parse::<Pattern>();
+        }
+
+        Ok(Literal("A".into()))
     }
+
+    // fn from_str(s: &str) -> Result<Self> {
+    //     // println!("parsing `{}`", s);
+
+    //     use self::Pattern::*;
+    //     let mut res: Vec<Pattern> = vec![];
+    //     let mut current = vec![];
+    //     let mut or_mode = false;
+    //     let mut chars = s.chars();
+
+    //     while let Some(c) = chars.next() {
+    //         match c {
+    //             '^' | '$' => {}
+    //             c @ 'N' | c @ 'S' | c @ 'E' | c @ 'W' => {
+    //                 current.push(c);
+    //             }
+    //             '(' => {
+    //                 if !current.is_empty() {
+    //                     res.push(Partial(current.iter().collect()));
+    //                     current = vec![];
+    //                 }
+
+    //                 let mut pending = vec![];
+    //                 let mut level = 0;
+    //                 while let Some(c) = chars.next() {
+    //                     if c == '(' {
+    //                         level += 1;
+    //                     }
+    //                     if c == ')' {
+    //                         if level == 0 {
+    //                             break;
+    //                         }
+    //                         level -= 1;
+    //                     }
+    //                     pending.push(c);
+    //                 }
+    //                 if !pending.is_empty() {
+    //                     res.push(Partial(pending.iter().collect()));
+    //                 }
+    //             }
+    //             '|' => {
+    //                 or_mode = true;
+    //                 if !current.is_empty() {
+    //                     res.push(Partial(current.iter().collect()));
+    //                     current = vec![];
+    //                 }
+    //                 let mut pending = vec![];
+    //                 let mut level = 0;
+    //                 while let Some(c) = chars.next() {
+    //                     if c == '(' {
+    //                         level += 1;
+    //                     }
+    //                     if c == ')' {
+    //                         if level == 0 {
+    //                             res.push(Partial(pending.iter().collect()));
+    //                             pending = vec![];
+    //                         }
+    //                         level -= 1;
+    //                     }
+    //                     pending.push(c);
+    //                 }
+    //                 if !pending.is_empty() {
+    //                     res.push(Partial(pending.iter().collect()));
+    //                 }
+    //             }
+    //             ')' => {
+    //                 if !current.is_empty() {
+    //                     res.push(Partial(current.iter().collect()));
+    //                     current = vec![];
+    //                 }
+    //             }
+    //             c => Err(err_msg(format!("parse fail: `{}`", c)))?,
+    //         }
+    //     }
+
+    //     let is_literal = |s: &str| {
+    //         s.chars()
+    //             .all(|c| c == 'N' || c == 'S' || c == 'E' || c == 'W')
+    //     };
+
+    //     if !current.is_empty() {
+    //         res.push(Partial(current.iter().collect()));
+    //     }
+
+    //     // let mode_str = if or_mode { "or" } else { "concat" };
+    //     // println!("res ({}): {:?}", mode_str, res);
+    //     let inside = res
+    //         .into_iter()
+    //         .map(|p| match p {
+    //             Partial(ref s) if is_literal(&s) => Literal(s.clone()),
+    //             Partial(s) => s.parse().expect("sub parsing"),
+    //             p => p,
+    //         })
+    //         // .filter(|p| if let Literal(s) = p && s == "" {false} else {true})
+    //         .collect();
+    //     let complete = if or_mode { Or(inside) } else { Concat(inside) };
+
+    //     println!("complete: {} -> {:?}", s, complete);
+    //     Ok(complete)
+    // }
 }
 
 #[cfg(test)]
@@ -260,8 +357,38 @@ mod tests {
             ])
         ))
     }
+
     #[test]
     fn test_parse3() -> Result<()> {
+        use self::Pattern::*;
+        Ok(assert_eq!(
+            "^E(N|S|W)$".parse::<Pattern>()?,
+            Concat(vec![
+                Literal("E".into()),
+                Or(vec![Literal("E".into()), Literal("N".into()), Literal("W".into())]),
+            ])
+        ))
+    }
+
+    // #[test]
+    fn test_parse4() -> Result<()> {
+        use self::Pattern::*;
+        Ok(assert_eq!(
+            "^E(N|S|)W$".parse::<Pattern>()?,
+            Concat(vec![
+                Literal("E".into()),
+                Or(vec![
+                    Literal("N".into()),
+                    Literal("S".into()),
+                    Literal("".into())
+                ]),
+                Literal("W".into()),
+            ])
+        ))
+    }
+
+    #[test]
+    fn test_parse5() -> Result<()> {
         use self::Pattern::*;
         Ok(assert_eq!(
             "^E(N|S(E|N))$".parse::<Pattern>()?,
@@ -278,7 +405,7 @@ mod tests {
         ))
     }
 
-    #[test]
+    // #[test]
     fn test_part1() -> Result<()> {
         Ok(assert_eq!(part1("")?, 0))
     }
